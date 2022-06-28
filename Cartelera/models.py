d1f02bfee43f2f992f3424072f148ed147de7cb6
ThomXII <thomaswilliamstw0@gmail.com>
2022-06-28 20:09:56 -0300

    Ultima Actualizacion

--- Cartelera/models.py
+++ Cartelera/models.py
@@ -1,4 +1,6 @@
 from cgitb import text
+from distutils.command.upload import upload
+from tabnanny import verbose
 from wsgiref import validate
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models 
@@ -12,6 +14,9 @@ class Actor(models.Model):
     nacimiento = models.DateField()
     bio = models.TextField(max_length=700)
 
+    class Meta:
+        verbose_name=('Actor')
+        verbose_name_plural=('Actores')
     def __str__(self):
         return self.nombre
 
@@ -22,33 +27,37 @@ class Director(models.Model):
     foto = models.ImageField(blank=True)
     nacimiento = models.DateField()
     bio = models.TextField(max_length=700)
-  
+    class Meta:
+        verbose_name=('Director')
+        verbose_name_plural=('Directores')
     def __str__(self):
         return self.nombre  
 
-
+#funcion para que me devuelva las 12 mejores peliculas
+class PeliculaManager(models.Manager):
+    def obtenerMejores(self):
+        return self.all().order_by('-puntaje')[:12]
 
 class Pelicula(models.Model):
+    objects = PeliculaManager()
     nombre = models.CharField(max_length=100,unique=True)
     genero = models.CharField(max_length=30)
     resumen = models.CharField(max_length=350)
+    foto= models.ImageField(null=True,blank=True,upload_to="Pelis")
     actores = models.ManyToManyField(Actor,related_name='peliculas')
     lanzamiento = models.PositiveIntegerField()
     director = models.ForeignKey('Director',on_delete=models.RESTRICT,related_name='peliculas')
-    puntaje = models.IntegerField(default=0,validators=[MinValueValidator(0),MaxValueValidator(5)])
-
-    def promedio(self):
-        promedio = 0
-        num = 0
-        den = 0
-        a=self.criticas_get.all()
-        self.save()
-        for i in self.a():
-            num+=i
-            den+=1
-        
-        promedio=0 if num == 0 else 1(num/(den))
-        self.puntaje =round(promedio)
+    puntaje = models.IntegerField(default=1,validators=[MinValueValidator(1),MaxValueValidator(5)])
+    """def para tener el promedio"""
+    def get_promedio(self):
+        juntarcriticas=self.critica_set.all()
+        cantidad_Criticas=0
+        sum=0
+        for c in juntarcriticas:
+            cantidad_Criticas+=1
+            sum+=c.puntaje
+        promedio=0 if (sum == 0) else (sum/cantidad_Criticas)
+        self.puntaje=promedio
         self.save()
     
     def __str__(self):
@@ -56,20 +65,19 @@ class Pelicula(models.Model):
     
 class Critica(models.Model):
     mail = models.EmailField(max_length=100,unique=True)
-    pelicula = models.ForeignKey('Pelicula', on_delete=models.CASCADE,related_name='criticas')
+    pelicula = models.ForeignKey('Pelicula', on_delete=models.CASCADE)
     puntaje = models.PositiveIntegerField(default=1,validators=[MinValueValidator(1), MaxValueValidator(5)])
     resenia = models.TextField(blank=True,max_length=400)
     validacion = models.BooleanField(default=True)
-    #falta una clase meta que ordene todo
     #sobrecargar metodo save
-    def borrar(self):
-        if self.validacion == False:
-            self.delete()
-
+    """def save(self):
+        critica=super().save()
+        self.pelicula.promedio()
+        return critica"""
     def save(self, *args, **kwargs):
-        if self.validacion == False:
-            self.delete()
-        super().save(*args, **kwargs)
+        guarda=super().save()
+        self.pelicula.get_promedio()
+        return guarda
 
 
     def __str__(self):
